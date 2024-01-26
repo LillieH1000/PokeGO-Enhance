e4ae44386627d1143a9ac11c6f0187e9332155fb
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-26 13:40:47 -0500

    Push changes before my laptop completely dies

--- studio/app/src/main/java/h/lillie/pokegotouch/Main.kt
+++ studio/app/src/main/java/h/lillie/pokegotouch/Main.kt
@@ -1,31 +1,57 @@
 package h.lillie.pokegotouch
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.GestureDetector
+import android.view.Gravity
 import android.view.MotionEvent
-import android.widget.RelativeLayout
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
-@SuppressLint("ClickableViewAccessibility")
+@SuppressLint("ClickableViewAccessibility","InflateParams")
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
 
-        val relativeLayout: RelativeLayout = findViewById(R.id.relativeLayout)
+        if (!Settings.canDrawOverlays(this@Main)) {
+            startActivityForResult(Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            ), 0)
+        } else {
+            val gestureDetector = GestureDetector(this@Main, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onDown(e: MotionEvent): Boolean {
+                    Log.d("Touch x", e.x.toString())
+                    Log.d("Touch y", e.y.toString())
+                    return true
+                }
+            })
 
-        val gestureDetector = GestureDetector(this@Main, object : GestureDetector.SimpleOnGestureListener() {
-            override fun onDown(e: MotionEvent): Boolean {
-                Log.d("Touch x", e.x.toString())
-                Log.d("Touch y", e.y.toString())
-                return true
+            val view = View(this@Main)
+            view.setOnTouchListener { _, event ->
+                gestureDetector.onTouchEvent(event!!)
             }
-        })
 
-        relativeLayout.setOnTouchListener { _, event ->
-            gestureDetector.onTouchEvent(event!!)
+            val windowManager: WindowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+
+            val windowManagerLayoutParams = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+            )
+            windowManagerLayoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+            windowManagerLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+            windowManagerLayoutParams.format = PixelFormat.TRANSLUCENT
+            windowManagerLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+
+            windowManagerLayoutParams.gravity = Gravity.TOP
+
+            windowManager.addView(view, windowManagerLayoutParams)
         }
     }
 }
