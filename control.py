ee6236ea4cb539a2cf7e283aab9c2130ff4c8001
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-30 20:51:22 -0500

    Add code comments

--- control.py
+++ control.py
@@ -7,8 +7,10 @@ openlimit = 0
 giftlimit = 0
 
 if (os.name == "nt"):
+    # Windows
     platform = ".\\w-tools\\adb.exe"
 elif (os.name == "posix"):
+    # Linux
     platform = "./l-tools/adb"
 
 if (len(parameters) > 1):
@@ -16,50 +18,67 @@ if (len(parameters) > 1):
 
 if (len(parameters) == 1):
     if (parameters[0] == "show"):
+        # Show Touches
         os.system(f"{platform} shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:1")
         sys.exit()
     elif (parameters[0] == "hide"):
+        # Hide Touches
         os.system(f"{platform} shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:0")
         sys.exit()
     elif (parameters[0] == "event"):
+        # Touch Events
         os.system(f"{platform} shell getevent -l")
         sys.exit()
     elif (parameters[0] == "res"):
+        # Device Resolution
         os.system(f"{platform} shell wm size")
         sys.exit()
     else:
         sys.exit()
 
 def opentap(x, y):
+    # Friend
     os.popen(f"{platform} shell input tap $((16#{x})) $((16#{y}))")
     time.sleep(3)
+    # Gift
     os.popen(f"{platform} shell input tap $((16#21c)) $((16#63f))")
     time.sleep(3)
+    # Open
     os.popen(f"{platform} shell input tap $((16#224)) $((16#7da))")
     time.sleep(1)
+    # Close
     os.popen(f"{platform} shell input tap $((16#221)) $((16#88f))")
     time.sleep(3)
+    # Close
     os.popen(f"{platform} shell input tap $((16#221)) $((16#88f))")
     time.sleep(3)
 
 def sendtap(x, y, hasgift):
+    # Friend
     os.popen(f"{platform} shell input tap $((16#{x})) $((16#{y}))")
     time.sleep(3)
     if (hasgift == "yes" or hasgift == "y"):
+        # Close
         os.popen(f"{platform} shell input tap $((16#221)) $((16#88f))")
         time.sleep(3)
+    # Send Gift
     os.popen(f"{platform} shell input tap $((16#130)) $((16#6c9))")
     time.sleep(3)
+    # Gift Image
     os.popen(f"{platform} shell input tap $((16#272)) $((16#34f))")
     time.sleep(3)
+    # Gift
     os.popen(f"{platform} shell input tap $((16#220)) $((16#7c5))")
     time.sleep(1)
+    # Close
     os.popen(f"{platform} shell input tap $((16#221)) $((16#88f))")
     time.sleep(3)
+    # Close
     os.popen(f"{platform} shell input tap $((16#221)) $((16#88f))")
     time.sleep(3)
 
 def scroll(open, gift, hasgift):
+    # Swipe
     os.popen(f"{platform} shell input touchscreen swipe 250 1000 250 500 400")
     time.sleep(1)
     friends(open, gift, hasgift)
@@ -117,7 +136,7 @@ def friends(open, gift, hasgift):
 print("")
 print("PokeGO Touch".center(os.get_terminal_size().columns))
 print("By LillieH1000".center(os.get_terminal_size().columns))
-print("Version: 4".center(os.get_terminal_size().columns))
+print("Version: 5".center(os.get_terminal_size().columns))
 print("")
 print("You can press ctrl+c to kill the script anytime in case of an error".center(os.get_terminal_size().columns))
 print("")
