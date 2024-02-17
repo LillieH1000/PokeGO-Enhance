f59f58943242f1e5c803588369577dc24506770e
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-02-16 23:42:49 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         // noinspection OldTargetApi, EditedTargetSdkVersion
         targetSdk = 33
         compileSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         vectorDrawables {
             useSupportLibrary = true
         }
